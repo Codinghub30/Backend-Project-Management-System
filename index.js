2b08eb809fe92b4b788d285592e2eaa5677923d6
Anup kumar chakra <anupkumarchakra30@gmail.com>
2024-07-09 22:02:49 +0530

    updated index.js

--- index.js
+++ index.js
@@ -2,55 +2,54 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import authRoute from './route/authRoute.js';
+// import taskRoute from './route/taskRoute.js';
 import taskRoute from './route/ProjectRoute.js';
 import cors from 'cors';
 
+
 const app = express();
-
-dotenv.config();
-
-// CORS configuration
-const allowedOrigins = [
-    'https://backend-project-management-system-1.onrender.com',
-    'http://localhost:3000',
-    'https://668d5102fdccce81c76c2831--brilliant-pastelito-c99b45.netlify.app'
-];
-
-const corsOptions = {
+const allowedOrigins = ['*'];
+  
+  // Configure the CORS options
+  const corsOptions = {
     origin: (origin, callback) => {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error('Not allowed by CORS'));
-        }
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
     },
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Authorization'
-};
+  };
+  
+  // Use the CORS middleware
+  app.use(cors(corsOptions));
 
-app.use(cors(corsOptions));
+dotenv.config();
 
-// MongoDB connection
-mongoose.connect(process.env.MONGO, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
+
+
+mongoose.connect(process.env.MONGO)
 .then(() => {
     console.log('MongoDb is connected');
 })
 .catch((err) => {
-    console.error('MongoDb connection error:', err);
-});
+    console.log(err);
+})
 
-// Middleware
+
 app.use(express.json());
+app.use(cors());
 
-// Routes
 app.use('/api/auth', authRoute);
 app.use('/api/project', taskRoute);
 
-// Start server
-const PORT = process.env.PORT || 9004;
+// app.use('/api/projects', projectRoute);
+
+
+
+const PORT = process.env.PORT||9004;
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+    console.log(`The server is running at ${PORT}`);
+})
